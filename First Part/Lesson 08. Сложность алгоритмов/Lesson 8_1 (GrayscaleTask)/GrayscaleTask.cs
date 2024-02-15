b4dc9f96aaf672803de9cb81c7c87c967c461afb
Lysko Denis <denislysko@yandex.ru>
2024-02-15 07:43:10 +0300

    feat: Первые полтора задания 8-го урока

---
+++ First Part/Lesson 08. Сложность алгоритмов/Lesson 8_1 (GrayscaleTask)/GrayscaleTask.cs
@@ -0,0 +1,20 @@
+﻿namespace Recognizer;
+
+public static class GrayscaleTask
+{
+	public static double[,] ToGrayscale(Pixel[,] original)
+	{
+		var width = original.GetLength(0);
+		var height = original.GetLength(1);
+		var grayscale = new double[width,height];
+
+		for (int i = 0; i < width; i++)
+			for (int j = 0; j < height; j++)
+			{
+				var currentPixel = original[i,j];
+				grayscale[i,j] = (0.299 * currentPixel.R + 0.587*currentPixel.G + 0.114 * currentPixel.B) / 255;
+			}
+
+		return grayscale;
+	}
+}
