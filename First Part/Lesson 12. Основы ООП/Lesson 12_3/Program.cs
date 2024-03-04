206aa57895192edd8e152326eb3c20b2f3eb23ca
Lysko Denis <denislysko@yandex.ru>
2024-03-05 01:46:12 +0300

    feat: Небольшие задания в самом уроке

---
+++ First Part/Lesson 12. Основы ООП/Lesson 12_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Lesson_12_3;
+
+public static class StringExtension
+{
+    public static int ToInt(this string s)
+        => int.TryParse(s, out int result)
+        ? result
+        : 0;
+}
+internal class Program
+{
+
+    public static void Main()
+    {
+        var arg1 = "100500";
+        Console.Write(arg1.ToInt() + "42".ToInt()); // 100542
+    }
+}
---
+++ First Part/Lesson 12. Основы ООП/Lesson 12_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Lesson_12_5;
+
+internal class Program
+{
+    public class SuperBeautyImageFilter
+    {
+        public string ImageName;
+        public double GaussianParameter;
+        public void Run()
+        {
+            Console.WriteLine("Processing {0} with parameter {1}",
+                ImageName,
+                GaussianParameter.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+    public static void Main()
+    {
+        var filter = new SuperBeautyImageFilter();
+        filter.ImageName = "Paris.jpg";
+        filter.GaussianParameter = 0.4;
+        filter.Run();
+    }
+}
