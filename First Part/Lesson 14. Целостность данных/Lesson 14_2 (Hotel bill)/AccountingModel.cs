ad2998229a7aa6f0f3c58ce48eaa0c9b3e2d1f96
DenLysko <denislysko@yandex.ru>
2024-05-19 19:00:18 +0300

    feat: practice Hotel bill (undone)

---
+++ First Part/Lesson 14. Целостность данных/Lesson 14_2 (Hotel bill)/AccountingModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace HotelAccounting;
+
+public class AccountingModel : ModelBase
+{
+    private int nightsCount;
+
+    private double total;
+    public double Price 
+    {
+        get { return Price; }
+        set
+        {
+            SetWithNotify(Price, value);
+        } 
+    }
+    public int NightsCount
+    {
+        get { return NightsCount; }
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException();
+            SetWithNotify(NightsCount, value);
+        }
+    }
+    
+    public double Discount 
+    {
+        get { return Discount; }
+        set
+        {
+            SetWithNotify(Discount, value);
+        } 
+    }
+
+    public double Total
+    {
+        get { return total; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException();
+            SetWithNotify(Total, value);
+        }
+    }
+
+    private void SetWithNotify(object property, object value)
+    {
+        property = value;
+        Notify(nameof(property));
+    }
+}
